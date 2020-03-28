69b52044606b3c512dbfe03453f3ba5977049694
unknown <Carlos Escriche>
2020-03-28 12:41:33 +0100

    Added layers

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -4,9 +4,10 @@
 // For use by Hazel applications
 
 #include <stdio.h>
-#include "Hazel\Application.h"
-#include "Hazel\Log.h"
+#include "Hazel/Application.h"
+#include "Hazel/Layer.h"
+#include "Hazel/Log.h"
 
 // ----Entry Point------------------
-#include "Hazel\EntryPoint.h"
+#include "Hazel/EntryPoint.h"
 // ---------------------------------
