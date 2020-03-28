30bf080db6eedefa4382e75c3b0b8ba5ffe9236d
unknown <Carlos Escriche>
2020-03-28 23:27:24 +0100

    Added input polling

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,10 +3,10 @@
 
 // For use by Hazel applications
 
-#include <stdio.h>
 #include "Hazel/Application.h"
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
+
 #include "Hazel/ImGui/ImGuiLayer.h"
 
 // ----Entry Point------------------
--- Hazel/src/Hazel/Platform/WindowsWindow.h
+++ Hazel/src/Hazel/Platform/WindowsWindow.h
@@ -17,17 +17,20 @@ namespace Hazel {
 		inline unsigned int GetWidth() const override { return m_Data.Width; }
 		inline unsigned int GetHeight() const override { return m_Data.Height; }
 
-		// Window Attributes
+		// Window attributes
 		inline void SetEventCallback(const EventCallbackFn& callback) override { m_Data.EventCallback = callback; }
 		void SetVSync(bool enabled) override;
 		bool IsVSync() const override;
+
+		inline virtual void* GetNativeWindow() const { return m_Window; }
 	private:
 		virtual void Init(const WindowProps& props);
 		virtual void Shutdown();
 	private:
 		GLFWwindow* m_Window;
 
-		struct WindowData {
+		struct WindowData
+		{
 			std::string Title;
 			unsigned int Width, Height;
 			bool VSync;
@@ -37,4 +40,5 @@ namespace Hazel {
 
 		WindowData m_Data;
 	};
+
 }
